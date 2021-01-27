7368c015d92618ce7cd448a9d59c805ad1271f1c
Tyler Cook <tyler.cook@self.inc>
2021-01-27 11:48:31 -0600

    Refactored star rating component implementation to be cleaner post class

--- StarRating.js
+++ StarRating.js
@@ -1,24 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Image, View, Button} from 'react-native';
 import PropTypes from 'prop-types';
 
 import greyStar from './Assets/Star_Grey.png';
 import purpleStar from './Assets/Star_Purple.png';
 
-class StarRating extends Component {
-  render() {
-    const renderedStars = [];
-    for (let i = 0; i < 10; i++) {
-      const ratingIndex = this.props.rating - 1;
-      const imageSource = i <= ratingIndex ? purpleStar : greyStar;
-      renderedStars.push(
-        <Button key={i} onPress={this.props.onChange.bind(this, i + 1)}>
-          <Image source={imageSource} />
-        </Button>,
-      );
-    }
-    return <View>{renderedStars}</View>;
-  }
+const stars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function StarRating(props) {
+  const renderedStars = stars.map((star) => {
+    const imageSource = star <= props.rating ? purpleStar : greyStar;
+
+    return (
+      <Button key={star} onPress={props.onChange.bind(this, star)}>
+        <Image source={imageSource} />
+      </Button>
+    );
+  });
+  return <View>{renderedStars}</View>;
 }
 
 StarRating.propTypes = {
