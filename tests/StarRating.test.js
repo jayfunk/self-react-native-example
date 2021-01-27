007438a55a2b6ebea4acb97246fdc5b23d971239
Tyler Cook <tyler.cook@self.inc>
2021-01-27 11:02:57 -0600

    Refactored tests based on the class as better examples for the next class

--- tests/StarRating.test.js
+++ tests/StarRating.test.js
@@ -1,46 +1,38 @@
+import React from 'react';
+
 import {Image, Button} from 'react-native';
 import {shallow} from 'enzyme';
 
 import StarRating from '../StarRating';
 
 describe('Star Rating', () => {
-  test('should render stars', () => {
-    const wrapper = shallow(<StarRating onChange={jest.fn()} />);
+  test('should render ten grey stars when the rating is zero', () => {
+    const wrapper = shallow(<StarRating onChange={jest.fn()} rating={0} />);
 
-    const stars = wrapper.find(Button);
+    const greyStars = wrapper
+      .find(Image)
+      .filterWhere((n) => n.prop('source').testUri.includes('Star_Grey.png'));
 
-    expect(stars.length).toEqual(10);
+    expect(greyStars.length).toEqual(10);
+  });
 
-    const starImage = stars.first().find(Image);
+  test('should render purple stars equal to the rating when given a rating greater than zero', () => {
+    const wrapper = shallow(<StarRating onChange={jest.fn()} rating={4} />);
 
-    expect(starImage.prop('source').testUri).toMatch('Star_Grey.png');
+    const purpleStars = wrapper
+      .find(Image)
+      .filterWhere((n) => n.prop('source').testUri.includes('Star_Purple.png'));
+
+    expect(purpleStars.length).toEqual(4);
   });
 
   test('should call the onChange callback when a star is pressed', () => {
     const mockOnChange = jest.fn();
-    const wrapper = shallow(<StarRating onChange={mockOnChange} />);
+    const wrapper = shallow(<StarRating onChange={mockOnChange} rating={0} />);
 
     const buttons = wrapper.find(Button);
-    buttons.last().prop('onPress')();
+    buttons.at(6).prop('onPress')();
 
-    expect(mockOnChange).toHaveBeenCalledWith(10);
-  });
-
-  test('should render the number of purple stars equal to the rating when given a rating', () => {
-    const wrapper = shallow(<StarRating onChange={jest.fn()} rating={4} />);
-
-    const starImages = wrapper.find(Image);
-
-    expect(starImages.at(0).prop('source').testUri).toMatch('Star_Purple.png');
-    expect(starImages.at(1).prop('source').testUri).toMatch('Star_Purple.png');
-    expect(starImages.at(2).prop('source').testUri).toMatch('Star_Purple.png');
-    expect(starImages.at(3).prop('source').testUri).toMatch('Star_Purple.png');
-
-    expect(starImages.at(4).prop('source').testUri).toMatch('Star_Grey.png');
-    expect(starImages.at(5).prop('source').testUri).toMatch('Star_Grey.png');
-    expect(starImages.at(6).prop('source').testUri).toMatch('Star_Grey.png');
-    expect(starImages.at(7).prop('source').testUri).toMatch('Star_Grey.png');
-    expect(starImages.at(8).prop('source').testUri).toMatch('Star_Grey.png');
-    expect(starImages.at(9).prop('source').testUri).toMatch('Star_Grey.png');
+    expect(mockOnChange).toHaveBeenCalledWith(7);
   });
 });
